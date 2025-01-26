a8df93d4dad8a2737dd9504ae860257eedf4e9e6
ChickenSir <shyguyman11@gmail.com>
2025-01-26 19:38:19 +1100

    Fix error with command arguments

--- src/java/simplecommandsystem/CommandSystem.java
+++ src/java/simplecommandsystem/CommandSystem.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -16,11 +17,11 @@ public class CommandSystem {
             String input = console.readLine();
 
             List<String> cmd = Arrays.asList(input.split(" ", 2));
-            List<String> cmdArgs = Arrays.asList(cmd.get(1).split(" "));
+            List<String> cmdArgs = (cmd.size() != 1) ? Arrays.asList(cmd.get(1).split(" ")) : new ArrayList<String>();
             Command cmdType = commandList.get(cmd.get(0));
 
             if (cmdType != null) {
-                System.out.println(cmdType.run(null));
+                System.out.println(cmdType.run(cmdArgs));
             } else {
                 System.out.println("[ERROR] Unknown command '" + cmd.get(0) + "'. Enter 'help' for a list of commands.");
             }
