87daf4c7a913272ec1c187f2c95a4a784a546714
ChickenSir <shyguyman11@gmail.com>
2025-01-26 19:41:59 +1100

    Add comments

--- src/java/simplecommandsystem/CommandSystem.java
+++ src/java/simplecommandsystem/CommandSystem.java
@@ -14,16 +14,22 @@ public class CommandSystem {
         Console console = System.console();
 
         while(true) {
+            // Get input from the user
             System.out.print("> ");
             String input = console.readLine();
 
+            // Split input into command name and arguments
             List<String> cmd = Arrays.asList(input.split(" ", 2));
             List<String> cmdArgs = (cmd.size() != 1) ? Arrays.asList(cmd.get(1).split(" ")) : new ArrayList<String>();
+
+            // Get command from command list
             Command cmdType = commandList.get(cmd.get(0));
 
             if (cmdType != null) {
+                // Run command from command list with arguments
                 System.out.println(cmdType.run(cmdArgs));
             } else {
+                // Display error if command does not exist
                 System.out.println("[ERROR] Unknown command '" + cmd.get(0) + "'. Enter 'help' for a list of commands.");
             }
         }
